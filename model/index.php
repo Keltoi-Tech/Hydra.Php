9cf0a8d4b4ce2eb34b8bb6453606df90bbf5e871
KeltoiCode <80556611+Keltoi-Tech@users.noreply.github.com>
2021-07-31 14:08:57 -0300

    Resetar model
    Resetar arquivos da model que trouxe da Hydra.Info

--- model/index.php
+++ model/index.php
@@ -1,9 +1,2 @@
 <?php
-    include_once("person.php");
-    include_once("locale.php");
-    include_once("address.php");
-    include_once("contactType.php");
-    include_once("contact.php");
-    include_once("documentType.php");
-    include_once("document.php");
 ?>
